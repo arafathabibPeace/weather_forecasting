47d47f6a1b85a0c27e8aa46f0bc031adedad8cbb
arafathabibPeace <arafathabibPeace@gmail.com>
2023-04-23 15:08:10 +0800

    Add server

---
+++ server/server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var cors = require('cors');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+
+var bodyParser = require('body-parser')
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+const CLIENT_ID = 'df2c553ef299ed4dfee9'
+const CLIENT_SECRET = 'b14758aab6105a16df740d44c663507561712fe8'
+
+app.get('/getAccessToken', async function (req, res) {
+
+    console.log(req.query.code, CLIENT_ID, CLIENT_SECRET)
+    const params = '?client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&code=' + req.query.code;
+    await fetch('https://github.com/login/oauth/access_token' + params, {
+        method: 'POST',
+        headers: {
+            "Accept": "application/json"
+        }
+    }).then((response) => {
+        return response.json()
+    }).then((data) => {
+        res.json(data)
+    }).catch((err) => {
+        console.log('error: ', err)
+    })
+})
+
+//Access token  is going to be passed in as an Authorization header
+app.get('/getUserData', async function (req, res) {
+    req.get('Authorization')
+    await fetch('https://api.github.com/user', {
+        method: 'GET',
+        headers: {
+            'Authorization': req.get('Authorization')
+        }
+    }).then((response) => {
+        return response.json()
+    }).then((data) => {
+        console.log(data)
+        res.json(data)
+    })
+})
+app.listen(4000, () => { console.log('CORS running on port 4000') })
